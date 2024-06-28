f412e8d4dff268cf7a258cc0b689648c3f9e55f2
Mo Faizan <fk29837@gmail.com>
2024-06-28 23:23:24 +0530

    Second Commit

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
 const port = 8000;
+const path = require("path");
 
 const URL = require("./models/url");
 const urlRoute = require("./routes/url");
+const staticRouter = require("./routes/staticRouter");
 
 const { connectToMongoDB } = require("./connect");
 
@@ -12,10 +14,15 @@ connectToMongoDB("mongodb://localhost:27017/short-url").then(() => {
 });
 
 app.use(express.json()); // Parse JSON bodies
+app.use(express.urlencoded({ extended: false }));
+
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
 
 app.use("/url", urlRoute);
+app.use("/", staticRouter);
 
-app.get("/:shortId", async (req, res) => {
+app.get("/url/:shortId", async (req, res) => {
   const shortId = req.params.shortId;
   const entry = await URL.findOneAndUpdate(
     { shortId },
